3988623b7cea0b2a3b4806860731006822233b94
dzakyy04 <dzakylinggau@gmail.com>
2023-08-05 18:58:51 +0700

    Create work program migration and make seeder for 19 work programs

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -21,7 +22,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             DepartementSeeder::class,
-            DivisionSeeder::class
+            DivisionSeeder::class,
+            WorkProgramSeeder::class,
         ]);
     }
 }

18dc3415add757ea5f9ff8fc031c22d64bd56239
Farhan Nugraha <farhan.14092003@gmail.com>
2023-08-10 21:14:23 +0700

    add form required

--- app/Http/Controllers/Api/IfCaringController.php
+++ app/Http/Controllers/Api/IfCaringController.php
@@ -11,7 +11,14 @@ class IfCaringController extends Controller
     public function submitIfCaring(Request $request)
     {
         $validatedData = $request->validate([
-            
+            'id' => 'required|integer',
+            'email' => 'required|email',
+            'full_name' => 'required|string',
+            'nim' => 'required|string',
+            'generation' => 'required|integer',
+            'class' => 'required|string',
+            'whatsapp_number' => 'required|string',
+            'aspiration_form' => 'rquired|string|max:1000'
         ]);
         $ifCaringData = IfCaring::create($validatedData);
 

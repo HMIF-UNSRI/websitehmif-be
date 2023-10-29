aeafd8a12c219e6c7a0a045ed802af296d6ce360
imancrrzii <imancarrazi777@gmail.com>
2023-10-29 11:08:48 +0700

    Add Login Feature to Admin

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\Admin\AuthController;
+use App\Http\Controllers\Admin\ArticleController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Admin\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,13 +20,23 @@ Route::get('/', function () {
     return redirect()->route('dashboard.index');
 });
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
 
-Route::get('/berita', [ArticleController::class, 'index'])->name('article.index');
-Route::get('/berita/create', [ArticleController::class, 'create'])->name('article.create');
-Route::post('/berita', [ArticleController::class, 'store'])->name('article.store');
-Route::get('/berita/{slug}/show', [ArticleController::class, 'show'])->name('article.show');
-Route::get('/berita/{slug}/edit', [ArticleController::class, 'edit'])->name('article.edit');
-Route::put('/berita/{slug}', [ArticleController::class, 'update'])->name('article.update');
-Route::delete('/berita/{slug}', [ArticleController::class, 'delete'])->name('article.delete');
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'index'])->name('login.index');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+    Route::get('/berita', [ArticleController::class, 'index'])->name('article.index');
+    Route::get('/berita/create', [ArticleController::class, 'create'])->name('article.create');
+    Route::post('/berita', [ArticleController::class, 'store'])->name('article.store');
+    Route::get('/berita/{slug}/show', [ArticleController::class, 'show'])->name('article.show');
+    Route::get('/berita/{slug}/edit', [ArticleController::class, 'edit'])->name('article.edit');
+    Route::put('/berita/{slug}', [ArticleController::class, 'update'])->name('article.update');
+    Route::delete('/berita/{slug}', [ArticleController::class, 'delete'])->name('article.delete');
+});
+
 
